efe3877592003718e39470b7a2c9fdf8a0c83a51
nesterovski <63017385+nesterovski@users.noreply.github.com>
2022-11-15 02:18:31 +0300

    js methods

--- js_beginning/start.js
+++ js_beginning/start.js
@@ -1,3 +1,19 @@
 hey = 5;
 console.log (hey);
 console.log ('hey friend');
+
+
+// ниже буду тренироваться использовать методы для строк
+
+let info = "this is my first code using methods";
+console.log(info);
+
+// Метод toUpperCase() используется для перевода всей строки в верхний регистр.
+
+let CapsInfo = info.toUpperCase();
+console.log(CapsInfo);
+
+// Метод replace() используется для замены части строки другой строкой. Этот метод чувствителен к регистру.
+
+let ReplaceInfo = info.replace("code", "replacement");
+console.log(ReplaceInfo);
