50671b652db3a677f96b94500069db2047072096
Eugene <mydeepsix@ya.ru>
2022-11-15 11:45:59 +0300

    checking if my parents' pc working

--- js_beginning/start.js
+++ js_beginning/start.js
@@ -1,6 +1,6 @@
 hey = 5;
-console.log (hey);
-console.log ('hey friend');
+console.log(hey);
+console.log('hey friend');
 
 
 // ниже буду тренироваться использовать методы для строк
@@ -13,7 +13,7 @@ console.log(info);
 let CapsInfo = info.toUpperCase();
 console.log(CapsInfo);
 
-// Метод replace() используется для замены части строки другой строкой. Этот метод чувствителен к регистру.
+// Метод replace() используется для замены части строки другой строкой. Этот метод чувствителен к регистру. Тут новая правка
 
 let ReplaceInfo = info.replace("code", "replacement");
 console.log(ReplaceInfo);
